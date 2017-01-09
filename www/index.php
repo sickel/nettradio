d08402ee05202140dc8c5a538423cafd85cc0674
Morten Sickel <morten@sickel.net>
2017-01-09 21:02:03 +0100

    Better handling of array. Stores active channel to read by other clients

--- www/index.php
+++ www/index.php
@@ -4,39 +4,46 @@ $ch=$_GET['ch'];
 // echo($ch);
 echo('<br/>');
 $chs=array(
-'p1'=>'http://lyd.nrk.no/nrk_radio_p1_ostlandssendingen_mp3_h',
-'p2'=>'http://lyd.nrk.no/nrk_radio_p2_mp3_h',
-'p3'=>'http://lyd.nrk.no/nrk_radio_p3_mp3_h',
-'p13'=>'http://lyd.nrk.no/nrk_radio_p13_mp3_h',
-'p1pluss'=>'http://lyd.nrk.no/nrk_radio_p1pluss_mp3_h',
-'jazz'=>'http://lyd.nrk.no/nrk_radio_jazz_mp3_h',
-'klassisk'=>'http://lyd.nrk.no/nrk_radio_klassisk_mp3_h',
-'nyheter'=>'http://lyd.nrk.no/nrk_radio_alltid_nyheter_mp3_h',
-'mp3'=>'http://lyd.nrk.no/nrk_radio_mp3_mp3_h'
+'p1'=>array('NRK P1','http://lyd.nrk.no/nrk_radio_p1_ostlandssendingen_mp3_h'),
+'p2'=>array('NRK P2','http://lyd.nrk.no/nrk_radio_p2_mp3_h'),
+'p3'=>array('NRK P3','http://lyd.nrk.no/nrk_radio_p3_mp3_h'),
+'p13'=>array('NRK P13','http://lyd.nrk.no/nrk_radio_p13_mp3_h'),
+'p1pluss'=>array('NRK P1+','http://lyd.nrk.no/nrk_radio_p1pluss_mp3_h'),
+'jazz'=>array('NRK alltid jazz','http://lyd.nrk.no/nrk_radio_jazz_mp3_h'),
+'klassisk'=>array('NRK alltid klassisk','http://lyd.nrk.no/nrk_radio_klassisk_mp3_h'),
+'nyheter'=>array('NRK alltid nyheter','http://lyd.nrk.no/nrk_radio_alltid_nyheter_mp3_h'),
+'mp3'=>array('NRK mp3','http://lyd.nrk.no/nrk_radio_mp3_mp3_h')
 );
-
+$file='ch.txt';
 if(array_key_exists($ch,$chs)){
-	`/usr/local/bin/radio.sh $chs[$ch]`;
+  $stream=$chs[$ch][1];
+  `/usr/local/bin/radio.sh $stream`;
+  $f=fopen($file,"w");
+  fwrite($f,$ch);
+  fclose($f);
 }else{
-
+   $f=fopen($file,"r");
+   if($f){
+     $ch=fread($f,filesize($file));
+     fclose($f);
+   }
 }
 if($_GET['off']=='Av'){
 	`/usr/local/bin/radio.sh off`;	
 }
 ?>
-
-<html><head>Nettradio</head><body>
+<html><head><title>Nettradio</title></head><body>
 <form action="" method="get">
 <select name="ch">
-<option value="p1">NRK P1</option>
-<option value="p2">NRK P2</option>
-<option value="p3">NRK P3</option>
-<option value="p13">NRK P13</option>
-<option value="p1pluss">NRK P1+</option>
-<option value="jazz">NRK alltid jazz</option>
-<option value="klassisk">NRK alltid klassisk</option>
-<option value="mp3">NRK mp3</option>
-<option value="nyheter">NRK alltid nyheter</option>
+<?php
+foreach($chs as $k=>$v){
+    $sel="";
+    if($k==$ch){
+        $sel="selected";
+    }
+    print("<option $sel value=\"{$k}\">{$v[0]}</option>\n");
+}
+?>
 </select>
 <input type="submit" value="Velg" />
 <input name="off" type="submit" value="Av" />
