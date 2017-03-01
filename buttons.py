6736aad2609004b119eaa59079e7f5931e8c9baa
Morten Sickel <morten@sickel.net>
2017-03-01 17:43:11 +0000

    Two buttons - state change - debounching

--- buttons.py
+++ buttons.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 
+import time
 
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.IN)
+GPIO.setup(22,GPIO.IN)
+prev17=0
+prev22=0
+
+
 while True:
-  if (GPIO.input(17)):
-    print("Button Pressed")
+  in17=GPIO.input(17)
+  in22=GPIO.input(22)
+  if(in17 and in17!=prev17):
+    print("Button 17 pressed")
+  prev17=in17
+  if(in22 and in22!=prev22):
+    print("Button 22 pressed")
+  prev22=in22
+  time.sleep(0.05)
