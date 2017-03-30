a6b0a77b137a4ea5cc96695d52da2e636b39cdc7
Morten Sickel <morten@sickel.net>
2017-03-30 19:20:14 +0000

    Fixing up various display issues

--- hardware/texttest.py
+++ hardware/texttest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
+# Copyright (c) 2017 Morten Sickel
+# 
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -21,11 +21,14 @@
 # THE SOFTWARE.
 
 import time
+import datetime
+import os.path
 import codecs
 from mikezlcd import *
 lcd = lcd_module(2004, 17, 4, 25, 24, 23,22)
 lcd.disp(0,3,"<-     ->       off ")
-clearline="                    "
+oldchname=""
+oldtext=""
 
 
 def getlast():
@@ -37,8 +40,11 @@ def getlast():
       if len(line) > 1:
          lastline=line
     lastline=lastline[:-3]
+    if "/usr/bin/jackd" in lastline:
+    # happens when changing canal
+       return("")
     splt=lastline.split('=')
-#  print(splt)
+    #  print(splt)
     if len(splt)>0:
        
       return splt[1].strip("'")
@@ -46,28 +52,55 @@ def getlast():
      lastline=""
   return(lastline)
 
-
+idx=0
 def showtext():
-  
+  # todo scroll text
+  # Check if unchanged if so, scroll one char to left. If last char is shown, start from first 
+  global oldchname
+  global oldtext
+  global idx
   ch="/var/www/html/ch2.txt"
   try:
     f=codecs.open(ch,encoding="utf-8")
     chname=f.readline()
   except:
     chname=""
-  lcd.disp(0,0,clearline)
-  lcd.disp(0,0,chname[:20])
-  txt=getlast().split('med')
-  # Write some text.
-  lcd.disp(0,1,clearline)
-  lcd.disp(0,1,txt[0][:20])
-  lcd.disp(0,2,clearline)
-  if len(txt) > 1:
-     txt[1].strip(" ") 
-     lcd.disp(0,2,txt[1][:20])
+  if chname!=oldchname:
+     lcd.disp(0,0,chname[:20].ljust(20))
+     # clears display if changed channel
+     lcd.disp(0,1," ".ljust(20))
+     lcd.disp(0,2," ".ljust(20))
+  oldchname=chname
+  filetext=getlast()
+  txt=filetext.split('med')
+  if filetext!=oldtext:
+     idx=0
+     lcd.disp(0,1,txt[0][:20].ljust(20))
+     if len(txt) > 1:
+        txt[1].strip(" ") 
+        lcd.disp(0,2,txt[1][:20].ljust(20))
+     else:
+        lcd.disp(0,2," ".ljust(20))
+  oldtext=filetext
+  if len(txt[0])>20:
+     lcd.disp(0,1,txt[0][idx:20+idx])
+#     print(txt[0][idx:20+idx])
+     if idx+20<len(txt[0]):
+        idx=idx+3
+     else:
+        idx=0
+     
 
-print 'Press Ctrl-C to quit.'
 while True:
-   lcd.disp(0,3,"<-     ->       off")
-   showtext()
-   time.sleep(5)
+   now=datetime.datetime.now()
+   lcd.disp(0,3,"<-  ->  off".ljust(20))
+   lcd.disp(15,3,str(now.hour))
+   lcd.disp(17,3,":")
+   lcd.disp(18,3,str(now.minute))
+   if os.path.isfile("/tmp/radiopid"):
+      showtext()
+   else:
+      lcd.disp(0,0," ".ljust(20))
+      lcd.disp(0,1," ".ljust(20))
+      lcd.disp(0,2," ".ljust(20))
+   time.sleep(1)
