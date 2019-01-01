a76431ead6e964d22fefbfba0717efd6ed711365
Morten Sickel <morten@sickel.net>
2019-01-01 16:14:59 +0100

    Corrected bug on selected item, added bbc2, cleaning and commenting

--- www/index.php
+++ www/index.php
@@ -1,25 +1,22 @@
 <html><head><title>Nettradio</title>
 <script type="text/javascript" src="radio.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1"></head>
-
 <body><br />
-
 <?php
-
-$file='ch.txt';
+$file='ch.txt'; 
 $file2='ch2.txt';
-//print_r($_GET);
 $chs=array();
+# Reading in stations from file:
 $file = fopen("stationlist.txt", "r") or exit("Unable to open file!");
 while(!feof($file))
-//Output a line of the file until the end is reached
   {
     $line=fgets($file);
     $list=explode(",",$line);
-    if($list[0]){
+    if(count($line)==3 && $list[0]){
         $chs[$list[0]]=[$list[1],$list[2]];}
   }
 fclose($file);
+
 $ch='';
 if(array_key_exists('ch',$_GET)){
     $ch=$_GET['ch'];
@@ -28,15 +25,10 @@ if(array_key_exists('ch',$_GET)){
    if($f){
      $ch=fread($f,filesize($file));
      fclose($f);
-  //   print("ch:$ch<-");
    }
-   //print("ch ok|$ch|<br/>");
    if(array_key_exists('browse',$_GET)){
-    //echo('browse');
       $keys=array_keys($chs);
-      //print_r($keys);
       $idx=array_search($ch,$keys);
-      //print($idx);
       if($idx===false){
         $ch=$keys[0];
      }else{
@@ -51,11 +43,10 @@ if(array_key_exists('ch',$_GET)){
             $idx=0;
             }
         $ch=$keys[$idx];
-        //echo($ch);
      }
    }
 }
-//echo($ch);
+
 if(array_key_exists($ch,$chs)){
   $stream=$chs[$ch][1];
   `/usr/local/bin/radio.sh $stream`;
@@ -76,7 +67,7 @@ if(array_key_exists('off',$_GET) && $_GET['off']=='Av'){
 foreach($chs as $k=>$v){
     $sel="";
     if($k==$ch){
-        $sel="selected";
+        $sel='selected="selected"';
     }
     print("<option $sel value=\"{$k}\">{$v[0]}</option>\n");
 }
@@ -85,7 +76,6 @@ foreach($chs as $k=>$v){
 <input type="submit" value="Velg" />
 <input name="off" type="submit" value="Av" />
 </form>
-
 <a href="?browse=prev">Forrige</a> 
 <a href="?browse=next">Neste</a>  
 <p>
@@ -93,4 +83,3 @@ Volum: <span id="volumevalue">0</span>% <button id="down" class="volbutton">-</b
 </p>
 <p id="radiotext"> </p>
 </body></html>
-
