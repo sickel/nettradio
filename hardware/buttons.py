f7fa666e0ef08dd7af9706550be59f8f4a19bdb3
Morten Sickel <morten@sickel.net>
2019-01-12 23:12:11 +0100

    more flexible if using alternative url for radio - made text output selectable for debugging / testing

--- hardware/buttons.py
+++ hardware/buttons.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 import time
-from subprocess import call
 import urllib2
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
@@ -9,9 +8,12 @@ GPIO.setup(9,GPIO.IN)
 GPIO.setup(10,GPIO.IN)
 GPIO.setup(11,GPIO.IN)
 
+debug=False
+
 prev9=1
 prev10=1
 prev11=1
+url="http://localhost/index.php"
 
 print "Ready"
 while True:
@@ -19,17 +21,18 @@ while True:
   in10=GPIO.input(10)
   in11=GPIO.input(11)
   if(in9 and in9!=prev9):
-    print("Button 9 pressed - next")
-    urllib2.urlopen("http://localhost/radio.php?browse=next")
-    #call(["wget","-q","--output-document=/dev/null","localhost/radio.php?browse=next"],shell=True)
+    if Debug:
+        print("Button 9 pressed - next")
+    urllib2.urlopen(url+"?browse=next")
   prev9=in9
   if(in10 and in10!=prev10):
-    print("Button 10 pressed - prev")
-    urllib2.urlopen("http://localhost/radio.php?browse=prev")
-    #call(["wget","-q -O /dev/null localhost/radio.php?browse=next"],shell=True)
+    if Debug:
+      print("Button 10 pressed - prev")
+    urllib2.urlopen(url+"?browse=prev")
   prev10=in10
   if(in11 and in11!=prev11):
-    print("Button 11 pressed - off")
+    if Debug:
+      print("Button 11 pressed - off")
     urllib2.urlopen("http://localhost/radio.php?off=Av")
   prev11=in11
   time.sleep(0.05)
