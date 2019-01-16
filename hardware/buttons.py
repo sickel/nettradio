ee47f3b89f7e45b58c68261ec0f37f7d88c99300
Morten Sickel <morten@sickel.net>
2019-01-16 20:27:41 +0100

    Some refactoring

--- hardware/buttons.py
+++ hardware/buttons.py
@@ -3,36 +3,43 @@
 import time
 import urllib2
 import RPi.GPIO as GPIO
+
+# TODO - use internal pulldowns or pullups and adjust code if needed
+
+btnext=9
+btback=10
+btoff=11
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(9,GPIO.IN)
-GPIO.setup(10,GPIO.IN)
-GPIO.setup(11,GPIO.IN)
+GPIO.setup(btnext,GPIO.IN)
+GPIO.setup(btback,GPIO.IN)
+GPIO.setup(btoff,GPIO.IN)
 
-debug=False
+debug=True
 
-prev9=1
-prev10=1
-prev11=1
+prevnext=1
+prevback=1
+prevoff=1
 url="http://localhost/index.php"
-
-print "Ready"
+if debug:
+  print "Ready"
 while True:
-  in9=GPIO.input(9)
-  in10=GPIO.input(10)
-  in11=GPIO.input(11)
-  if(in9 and in9!=prev9):
-    if Debug:
+  innext=GPIO.input(btnext)
+  inback=GPIO.input(btback)
+  inoff=GPIO.input(btoff)
+  if(innext and innext!=prevnext):
+    if debug:
         print("Button 9 pressed - next")
     urllib2.urlopen(url+"?browse=next")
-  prev9=in9
-  if(in10 and in10!=prev10):
-    if Debug:
+  prevnext=innext
+  if(inback and inback!=prevback):
+    if debug:
       print("Button 10 pressed - prev")
     urllib2.urlopen(url+"?browse=prev")
-  prev10=in10
-  if(in11 and in11!=prev11):
-    if Debug:
+  prevback=inback
+  if(inoff and inoff!=prevoff):
+    if debug:
       print("Button 11 pressed - off")
     urllib2.urlopen("http://localhost/radio.php?off=Av")
-  prev11=in11
-  time.sleep(0.05)
+  prevoff=inoff
+  time.sleep(0.05) # Debouncing
