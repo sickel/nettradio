ec1dbecb149376bafee35504e50d318a58452b0b
Morten Sickel <morten@sickel.net>
2025-03-23 07:11:16 +0000

    With direct output, to be called with popen

---
+++ shell/radio_output.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+path=/usr/bin:/bin
+url=$1
+pidfile=/tmp/radiopid
+
+
+if [ -f $pidfile ];
+then
+   kill `cat $pidfile`
+   rm $pidfile
+fi
+
+if [ $url != "off" ]; then
+   mpg123  --no-control $url &
+   echo $! > $pidfile
+fi
